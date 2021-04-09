7c0ca2b59ef146624160cdfe4d5a5e7fc742ddde
Jasmeet Rangar <jasmeetsinghr@gmail.com>
2021-04-09 16:52:06 -0400

    Fix display bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import {useApplicationData} from './hooks/useApplicationData';
+import useApplicationData from './hooks/useApplicationData';
 
 const App = () => {
   const {
       state,
       dispatch
   } = useApplicationData();
-    const userList = state.users.map((user) => (<li key={user.id} > {user.first_name} {user.last_name} {user.email} </li>
+    const userList = state.users.map((user) => (<li key={user.id} > {user.first_name} {user.last_name} {user.email} {user.display_name}</li>
 ));
 return (<div className="App" >
   <h1> Users </h1>
