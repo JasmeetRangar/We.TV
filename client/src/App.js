605dd8af7941d2919bd5cbaa57d91a7bddf5c640
Joseph Blais <josephpierreblais@gmail.com>
2021-04-11 13:07:00 -0400

    Add Mui Theme colors to app

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import useApplicationData from './hooks/useApplicationData';
 import Navbar from './components/Navbar'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import { green, orange } from '@material-ui/core/colors';
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+
+const theme = createMuiTheme({
+  palette: {
+    background: {
+      default: "#303030"
+    },
+    secondary: {
+      main: orange[500]
+    },
+    text: {
+      primary : green[500]
+    }
+  }
+});
 
 const App = () => {
   const {
@@ -11,12 +29,15 @@ const App = () => {
     const userList = state.users.map((user) => (<li key={user.id} > {user.first_name} {user.last_name} {user.email} {user.display_name}</li>
 ));
 return (
-<div className="App" >
-  <Navbar></Navbar>
-  <h1> Users </h1>
+  <div className="App" >
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar></Navbar>
+        <h1> Users </h1>
 
-  <ul> {userList} </ul>
-</div >
+        <ul> {userList} </ul>
+      </MuiThemeProvider>
+    </div >
 );
 };
 export default App;
