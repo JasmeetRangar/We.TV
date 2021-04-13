e1d1add56d483b1703fff67bdf4f281d071ff9c1
Joseph Blais <josephpierreblais@gmail.com>
2021-04-13 12:05:53 -0400

    add link to /profile route in nav

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,21 +1,22 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import AccountCircle from '@material-ui/icons/AccountCircle'
-import ExitToAppIcon from '@material-ui/icons/ExitToApp'
-import SearchIcon from '@material-ui/icons/Search'
-import TvIcon from '@material-ui/icons/Tv'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import SearchIcon from "@material-ui/icons/Search";
+import TvIcon from "@material-ui/icons/Tv";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
   Toolbar: {
-    display: 'flex',
-    justifyContent: 'space-around'
-  }
+    display: "flex",
+    justifyContent: "space-around",
+  },
 }));
 
 export default function Navbar() {
@@ -23,22 +24,24 @@ export default function Navbar() {
 
   return (
     <div className={classes.root}>
-    <AppBar position="fixed">
-      <Toolbar className={classes.Toolbar}>
-        <IconButton color="inherit" aria-label="shows">
-          <TvIcon />
-        </IconButton>
-        <IconButton color="inherit" aria-label="search">
-          <SearchIcon />
-        </IconButton>
-        <IconButton color="inherit" aria-label="account">
-          <AccountCircle />
-        </IconButton>
-        <IconButton color="inherit" aria-label="logout">
-          <ExitToAppIcon />
-        </IconButton>
-      </Toolbar>
-    </AppBar>
-  </div>
-  )
+      <AppBar position="fixed">
+        <Toolbar className={classes.Toolbar}>
+          <IconButton color="inherit" aria-label="shows">
+            <TvIcon />
+          </IconButton>
+          <IconButton color="inherit" aria-label="search">
+            <SearchIcon />
+          </IconButton>
+          <IconButton color="inherit" aria-label="account">
+            <Link to="/profile">
+              <AccountCircle />
+            </Link>
+          </IconButton>
+          <IconButton color="inherit" aria-label="logout">
+            <ExitToAppIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
