6d7c2b34a6ec301f41c71f3a33615b95319f5067
Ghulam Murtaza <Ghulam.murtaza@mail.mcgill.ca>
2021-04-15 01:53:10 +0500

    custom hook for application data updated

--- client/src/hooks/useApplicationData.js
+++ client/src/hooks/useApplicationData.js
@@ -10,18 +10,16 @@ export default function useApplicationData(id) {
   // });
   const [state, setState] = useState({
     users: [],
-    posts: [],
-    comments: [],
+    posts: []
   })
 
   useEffect(()=> {
     Promise.all([
       axios.get('/api/users'),
-      axios.get(`/api/posts/${id}`),
-      axios.get('/api/comments'),
+      axios.get(`/api/posts/${id}`)
       // axios.get('/api/interviewers')
     ]).then(all => {
-      setState(prev => ({...prev, users: all[0].data, posts: all[1].data, comments: all[2].data}))
+      setState(prev => ({...prev, users: all[0].data, posts: all[1].data}))
     })
     
   }, [])
