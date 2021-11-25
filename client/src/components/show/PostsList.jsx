98a9c156976b28de89cb725caf72f7030287ad07
Joseph Blais <josephpierreblais@gmail.com>
2021-11-25 15:37:27 -0500

    Format props list

--- client/src/components/show/PostsList.jsx
+++ client/src/components/show/PostsList.jsx
@@ -2,8 +2,16 @@ import React from "react";
 import PostCard from "./PostCard";
 
 export default function PostsList(props) {
-	const { posts, comments } = props;
-	return posts.map((post, index) => {
-		return (<PostCard comments={comments} post={post} index={index} likeHandler={props.likeHandler} dislikeHandler={props.dislikeHandler} />)
-	});
+  const { posts, comments } = props;
+  return posts.map((post, index) => {
+    return (
+      <PostCard
+        comments={comments}
+        post={post}
+        index={index}
+        likeHandler={props.likeHandler}
+        dislikeHandler={props.dislikeHandler}
+      />
+    );
+  });
 }
